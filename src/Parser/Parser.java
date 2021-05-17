fb9fa26d5fd9a6ddc8058d372eae742049306f64
Aaron Schapira <aaronschapira@gmail.com>
2021-05-17 17:37:25 +0200

    Update

---
+++ src/Parser/Parser.java
@@ -0,0 +1,29 @@
+package Parser;
+
+public class Parser {
+
+    String path;
+
+
+    public Parser(String path){
+
+        this.path = path;
+
+    }
+    
+    // Construct the message
+
+    // Method to get Username //AARON
+
+    // Method that goes through the message and check if we need to censure //CLÉMENT
+
+    // Filter the reactions
+
+    // Get reaction
+
+    // Get reactions reactions from deleted messages and check if one of the important guys is in it
+
+
+
+
+}
